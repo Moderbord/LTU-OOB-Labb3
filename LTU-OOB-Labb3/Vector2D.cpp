48bb641d5c4742d08a5fc2e75ae3afcca282e698
Pontus Andersson <ponane-6@student.ltu.se>
2018-12-04 12:40:57 +0100

    Multiple operator overloading implemented

--- LTU-OOB-Labb3/Vector2D.cpp
+++ LTU-OOB-Labb3/Vector2D.cpp
@@ -1,18 +1,80 @@
 #include "pch.h"
 #include "Vector2D.h"
+#include <iostream>
 
 
-Vector2D::Vector2D(float x, float y) {
-	arr[0] = x;
-	arr[1] = y;
+Vector2D::Vector2D()
+{
+	std::cout << "Empty constructor" << std::endl;
+	this->arr[0] = 0;
+	this->arr[1] = 0;
 }
 
-float Vector2D::getX()
+Vector2D::Vector2D(float x, float y) {
+	std::cout << "Constructor with values" << std::endl;
+	this->arr[0] = x;
+	this->arr[1] = y;
+}
+
+Vector2D::Vector2D(const Vector2D &vector)
+{
+	std::cout << "Copy constructor" << std::endl;
+	this->arr[0] = vector.arr[0];
+	this->arr[1] = vector.arr[1];
+}
+
+Vector2D Vector2D::operator+(const Vector2D &vector)
+{
+	std::cout << "+ operator" << std::endl;
+	Vector2D vec;
+	vec.arr[0] = this->arr[0] + vector.arr[0];
+	vec.arr[1] = this->arr[1] + vector.arr[1];
+	return vec;
+}
+
+Vector2D Vector2D::operator-(const Vector2D &vector)
+{
+	std::cout << "- operator" << std::endl;
+	Vector2D vec;
+	vec.arr[0] = this->arr[0] - vector.arr[0];
+	vec.arr[1] = this->arr[1] - vector.arr[1];
+	return vec;
+}
+
+Vector2D Vector2D::operator*(const Vector2D &vector)
+{
+	std::cout << "* operator" << std::endl;
+	Vector2D vec;
+	vec.arr[0] = this->arr[0] * vector.arr[0];
+	vec.arr[1] = this->arr[1] * vector.arr[1];
+	return vec;
+}
+
+void Vector2D::operator=(const Vector2D &vector)
+{
+	std::cout << "= operator" << std::endl;
+	this->arr[0] = vector.arr[0];
+	this->arr[1] = vector.arr[1];
+}
+
+bool Vector2D::operator==(const Vector2D &vector)
+{
+	std::cout << "== operator" << std::endl;
+	return (this->arr[0] == vector.arr[0] && this->arr[1] == vector.arr[1]) ? true : false;	// If both vector values are equivalent return true
+}
+
+bool Vector2D::operator!=(const Vector2D &vector)
+{
+	std::cout << "!= operator" << std::endl;
+	return (this->arr[0] != vector.arr[0] || this->arr[1] != vector.arr[1]) ? true : false;	// If any vector value isn't equivalent return true
+}
+
+const float Vector2D::getX()
 { 
 	return arr[0]; 
 }
 
-float Vector2D::getY()
+const float Vector2D::getY()
 { 
 	return arr[1];
 }
--- LTU-OOB-Labb3/Vector2D.h
+++ LTU-OOB-Labb3/Vector2D.h
@@ -4,9 +4,17 @@ class Vector2D
 private:
 	float arr[2];
 public:
+	Vector2D();
 	Vector2D(float x, float y);
-	float getX();
-	float getY();
+	Vector2D(const Vector2D &vector);
+	Vector2D operator+(const Vector2D &vector);
+	Vector2D operator-(const Vector2D &vector);
+	Vector2D operator*(const Vector2D &vector);
+	void operator=(const Vector2D &vector);
+	bool operator==(const Vector2D &vector);
+	bool operator!=(const Vector2D &vector);
+	const float getX();
+	const float getY();
 	void setX(float x);
 	void setY(float y);
 };
