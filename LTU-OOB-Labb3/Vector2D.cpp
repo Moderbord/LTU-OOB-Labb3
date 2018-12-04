9903a5870f4c4fd769eb2c978f8963774ad3e8ff
Pontus Andersson <ponane-6@student.ltu.se>
2018-12-04 13:59:29 +0100

    Vector2D class finished

--- LTU-OOB-Labb3/Vector2D.cpp
+++ LTU-OOB-Labb3/Vector2D.cpp
@@ -1,31 +1,33 @@
 #include "pch.h"
 #include "Vector2D.h"
 #include <iostream>
+#include <cmath>
 
+namespace {
+	using std::cout;
+	using std::endl;
+}
 
 Vector2D::Vector2D()
 {
-	std::cout << "Empty constructor" << std::endl;
 	this->arr[0] = 0;
 	this->arr[1] = 0;
 }
 
-Vector2D::Vector2D(float x, float y) {
-	std::cout << "Constructor with values" << std::endl;
+Vector2D::Vector2D(float x, float y)
+{
 	this->arr[0] = x;
 	this->arr[1] = y;
 }
 
 Vector2D::Vector2D(const Vector2D &vector)
 {
-	std::cout << "Copy constructor" << std::endl;
 	this->arr[0] = vector.arr[0];
 	this->arr[1] = vector.arr[1];
 }
 
 Vector2D Vector2D::operator+(const Vector2D &vector)
 {
-	std::cout << "+ operator" << std::endl;
 	Vector2D vec;
 	vec.arr[0] = this->arr[0] + vector.arr[0];
 	vec.arr[1] = this->arr[1] + vector.arr[1];
@@ -34,7 +36,6 @@ Vector2D Vector2D::operator+(const Vector2D &vector)
 
 Vector2D Vector2D::operator-(const Vector2D &vector)
 {
-	std::cout << "- operator" << std::endl;
 	Vector2D vec;
 	vec.arr[0] = this->arr[0] - vector.arr[0];
 	vec.arr[1] = this->arr[1] - vector.arr[1];
@@ -43,7 +44,6 @@ Vector2D Vector2D::operator-(const Vector2D &vector)
 
 Vector2D Vector2D::operator*(const Vector2D &vector)
 {
-	std::cout << "* operator" << std::endl;
 	Vector2D vec;
 	vec.arr[0] = this->arr[0] * vector.arr[0];
 	vec.arr[1] = this->arr[1] * vector.arr[1];
@@ -52,20 +52,17 @@ Vector2D Vector2D::operator*(const Vector2D &vector)
 
 void Vector2D::operator=(const Vector2D &vector)
 {
-	std::cout << "= operator" << std::endl;
 	this->arr[0] = vector.arr[0];
 	this->arr[1] = vector.arr[1];
 }
 
 bool Vector2D::operator==(const Vector2D &vector)
 {
-	std::cout << "== operator" << std::endl;
 	return (this->arr[0] == vector.arr[0] && this->arr[1] == vector.arr[1]) ? true : false;	// If both vector values are equivalent return true
 }
 
 bool Vector2D::operator!=(const Vector2D &vector)
 {
-	std::cout << "!= operator" << std::endl;
 	return (this->arr[0] != vector.arr[0] || this->arr[1] != vector.arr[1]) ? true : false;	// If any vector value isn't equivalent return true
 }
 
@@ -87,4 +84,40 @@ void Vector2D::setX(float x)
 void Vector2D::setY(float y)
 { 
 	arr[1] = y;
+}
+
+Vector2D Vector2D::computeVectorNormal()
+{
+	Vector2D vector;
+	float normal = this->computeVectorLength();
+
+	vector.arr[0] = this->arr[0] / normal;
+	vector.arr[1] = this->arr[1] / normal;
+
+	return vector;
+}
+
+float Vector2D::computeVectorLength()
+{
+	return sqrt(pow(this->arr[0], 2) + pow(this->arr[1], 2));		// sqrt(a^2 + b^2)
+}
+
+float Vector2D::computeDotProduct(const Vector2D & vector)
+{
+	return this->arr[0] * vector.arr[0] + this->arr[1] * vector.arr[1];		// a * x + b * y
+}
+
+const void Vector2D::printX()
+{
+	cout << "x: " << this->arr[0] << endl;
+}
+
+const void Vector2D::printY()
+{
+	cout << "y: " << this->arr[1] << endl;
+}
+
+const void Vector2D::printXY()
+{
+	cout << "x: " << this->arr[0] << ", y: " << this->arr[1] << endl;
 }
